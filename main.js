dc1ee2b250831e249b0e29707f12ff19ae66f581
Fariz <farizbakur@gmail.com>
2024-01-11 16:16:19 +0700

    mempersmooth animasi dan memperbaiki UI

--- main.js
+++ main.js
@@ -39,3 +39,13 @@ function toggleProductMenu(show) {
         productMenu.classList.add('opacity-0', 'scale-y-0');
     }
 }
+
+// hamburger
+function Menu(e) {
+    let list = document.querySelector('ul');
+
+    e.name === 'menu' ? (e.name = "close", list.classList.
+        add('top-[80px]'), list.classList.add('opacity-100')
+    ) : (e.name = "menu", list.classList.remove('top-[80px]')
+        , list.classList.remove('opacity-100'))
+}
