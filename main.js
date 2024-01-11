41fb388c8a3a53d86e163bc234edf348d7ff6c00
Fariz <farizbakur@gmail.com>
2024-01-11 15:26:03 +0700

    menambahkan fitur dark mode

---
+++ main.js
@@ -0,0 +1,41 @@
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+}
+
+// dropdown script
+function toggleProductMenu(show) {
+    const productMenu = document.querySelector('.product-menu');
+    if (show) {
+        productMenu.classList.remove('opacity-0', 'scale-y-0');
+        productMenu.classList.add('opacity-100', 'scale-y-100');
+    } else {
+        productMenu.classList.remove('opacity-100', 'scale-y-100');
+        productMenu.classList.add('opacity-0', 'scale-y-0');
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*.{html,js}"],
+  darkMode:'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#FFF',
+        primaryColorLight: '#FFF',
+        primaryColorTextLight: '#000',
+        primaryColorTextDark: '#fff',
+        headerColorLight: '#bfdbfe',
+        headerColorDark: '#0f0096',
+        secondaryColor: '#FFCC00',
+        paragraphColor: '#c0c0c0',      
+        whiteColor: '#fff',
+        blackColor: "#000",
+        greenColor: "#007936",
+        redColor: "#cc3433",
+        darkColor: '#000',
+        darkColorLight: "#171717",
+      }
+    },
   },
   plugins: [],
 }
